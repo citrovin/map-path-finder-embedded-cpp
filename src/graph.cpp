7822ee780a8c382748e01ee6af509709b1185483
citrovin <94009181+citrovin@users.noreply.github.com>
2023-03-12 13:48:31 +0100

    Graph library finished  v1 - may need optimization

--- src/graph.cpp
+++ src/graph.cpp
@@ -3,16 +3,17 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Vertex{
-    private:
+    public:
         int id_;
         double longitude_;
         double lattitude_;
 
-    public:
+    Vertex(){};
     Vertex(int id, double longitude, double lattitude){
         id_ = id;
         longitude_ = longitude;
@@ -20,24 +21,22 @@ class Vertex{
     }
 
     void const print_vertex(){
+        
         cout    << "| ID: " << id_ 
-                << "| Long.: " << longitude_ 
-                << "| Lat.: " << lattitude_
+                << "\t| Long.: " << longitude_ 
+                << "\t| Lat.: " << lattitude_
                 << "\n";
     }
 };
 
 class Edge{
-    private:
-        int id_;
+    public:
         int source_vid_;
         int dest_vid_;
         double length_;
         string name_;
 
-    public:
-    Edge(int id, int source_vid, int dest_vid, double length, string name){
-        id_ = id;
+    Edge(int source_vid, int dest_vid, double length, string name){
         source_vid_ =source_vid;
         dest_vid_ = dest_vid;
         length_ = length;
@@ -46,11 +45,33 @@ class Edge{
 
     void const print_edge(){
         cout    << "| Source ID: " << source_vid_ 
-                << "| Destination ID: " << dest_vid_
-                << "| Length: " << length_
-                << "| Name: " << name_
+                << "\t| Destination ID: " << dest_vid_
+                << "\t| Length: " << length_
+                << "\t| Name: " << name_
                 << "\n";
     }
+};
+
+
+class AdjacencyElement{
+    public:
+        Vertex v_;
+        vector<int> adjacent_elements_;
+
+        AdjacencyElement(Vertex v, vector<int> adjacent_elements){
+            v_=v;
+            adjacent_elements_=adjacent_elements;
+        }
+        AdjacencyElement(){};
+
+        void const print_adjacent_elements(){
+            cout << "Source Node: "<< v_.id_
+            << "\t | Destination Nodes: {";
+            for(int i : adjacent_elements_){
+                cout << i << " ";
+            }
+            cout << "}\n";
+        }
 };
 
 
@@ -58,31 +79,83 @@ class Graph{
     private:
         vector<Vertex> verteces_;
         vector<Edge> edges_;
-        // adjacency list!!!
+        
 
     public:
+    unordered_map<int, AdjacencyElement> adjacent_list_;
+
     Graph(vector<Vertex> verteces, vector<Edge> edges){
         verteces_ = verteces;
         edges_ = edges;
+
+        // create adjaceny list
+        for(Edge e : edges){
+            if(auto it = adjacent_list_.find(e.source_vid_); it!=adjacent_list_.end()){
+                // if node is already in the list, insert the destination node to adjacency list of corresponding node
+                it->second.adjacent_elements_.emplace_back(e.dest_vid_);
+                
+            }else{
+                // if node is not yet in the list, create new entry
+                vector<int> dest_nodes {e.dest_vid_};
+                AdjacencyElement adjacent;
+                
+                for(Vertex v : verteces){
+                    if(v.id_==e.source_vid_){
+                        adjacent = AdjacencyElement(v, dest_nodes);
+                    }
+                }
+
+                adjacent_list_.insert({e.source_vid_, adjacent});
+            }
+
+        }
+
+    }
+
+    void const summary(){
+        cout << "\nSummary of Graph:\n"
+            << "--------------\n" 
+            << "# Nodes: " << verteces_.size() << "\n"
+            << "# Edges: " << edges_.size() << "\n"
+            << "--------------\n";
+    }
+
+    void const print_adjacent_list(){
+        cout << "\nAdjacent list: \n"
+        << "---------------------------------------------------------" << endl;
+        for(auto it : adjacent_list_){
+            it.second.print_adjacent_elements();
+        }
+        cout << "---------------------------------------------------------" << endl;
     }
 
     void const print_verteces(){
         cout
-        << "Nodes:\n" 
-        << "-------------------------------------------------------------------------------\n";
-        for(Vertex v : verteces_) v.print_vertex();
+        << "Nodes\n" 
+        << "---------------------------------------------------------\n";
+        int count=0;
+        for(Vertex v : verteces_){
+            v.print_vertex();
+            count++;
+            }
         cout
-        << "-------------------------------------------------------------------------------\n";
+        << "---------------------------------------------------------\n"
+        << "Number of Verteces: " << count << endl;
     }
 
 
     void const print_edges(){
         cout
-        << "\nEdges:\n"
-        << "-------------------------------------------------------------------------------\n";
-        for(Edge e : edges_) e.print_edge();
+        << "\nEdges\n"
+        << "-------------------------------------------------------------------------------------------------\n";
+        int count = 0;
+        for(Edge e : edges_){
+            e.print_edge();
+            count++;
+        } 
         cout
-        << "-------------------------------------------------------------------------------\n";
+        << "-------------------------------------------------------------------------------------------------\n"
+        << "Number of Edges: " << count << endl;
     }
 
 
@@ -147,7 +220,7 @@ int main(int argc, char *argv[]){
             name = line.substr(0, line.find(','));
             line.erase(0, line.find(',')+1);
 
-            edges.emplace_back(Edge(id, source_vid, dest_vid, length, name));
+            edges.emplace_back(Edge(source_vid, dest_vid, length, name));
         }
         
         
@@ -155,7 +228,10 @@ int main(int argc, char *argv[]){
 
     Graph g = Graph(nodes, edges);
 
-    g.print_verteces();
-    g.print_edges();
+    // g.print_verteces();
+    // g.print_edges();
+
+    // g.print_adjacent_list();
+    g.summary();
     
 }
