c42de24fe9ea0a60e6bdd6a3aeae45a06edad077
citrovin <94009181+citrovin@users.noreply.github.com>
2023-03-12 20:18:16 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 int main(int argc, char *argv[]){
     Graph g = Graph(argv[1]);
 
-    g.print_verteces();
+    // g.print_verteces();
     // g.print_edges();
-
-    // g.print_adjacent_list();
-    // g.summary();
+    g.summary();
 }
