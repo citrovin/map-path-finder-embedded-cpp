618f17e50869fc55e1c9eaa2a8a3c9c971e6cce8
citrovin <94009181+citrovin@users.noreply.github.com>
2023-03-12 14:06:39 +0100

    graph library used as header in new header dir

--- src/headers/graph.hpp
+++ src/headers/graph.hpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -156,82 +154,5 @@ class Graph{
         cout
         << "-------------------------------------------------------------------------------------------------\n"
         << "Number of Edges: " << count << endl;
-    }
-
-
-        
+    }      
 };
-
-int main(int argc, char *argv[]){
-    ifstream file_stream;
-    string file_name{argv[1]};
-
-    file_stream.open(file_name, ios::in);
-    string line;
-
-    vector<Vertex> nodes;
-    vector<Edge> edges;
-
-    while (getline(file_stream, line)) {
-        if(line[0]=='#') continue;
-        std::istringstream stream(line);
-
-        char type='#';
-        int id=-1;
-
-        if(line[0]=='V'){
-            // load vertices (nodes)
-            double longitude;
-            double lattitude;
-            
-            line.erase(0, line.find(',')+1);
-
-            id = stoi(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            longitude = stod(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            lattitude = stod(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            nodes.emplace_back(Vertex(id, longitude, lattitude));
-        }
-
-        
-        if(line[0]=='E'){
-            // load edges
-            string name;
-            int source_vid;
-            int dest_vid;
-            double length;
-
-            line.erase(0, line.find(',')+1);
-
-            source_vid = stoi(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            dest_vid = stoi(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            length = stod(line.substr(0, line.find(',')));
-            line.erase(0, line.find(',')+1);
-
-            name = line.substr(0, line.find(','));
-            line.erase(0, line.find(',')+1);
-
-            edges.emplace_back(Edge(source_vid, dest_vid, length, name));
-        }
-        
-        
-    }
-
-    Graph g = Graph(nodes, edges);
-
-    // g.print_verteces();
-    // g.print_edges();
-
-    // g.print_adjacent_list();
-    g.summary();
-    
-}
