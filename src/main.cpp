6f1e4686513feabc962c5262c7b93296c5d7c662
citrovin <94009181+citrovin@users.noreply.github.com>
2023-03-18 12:10:31 +0100

    argument parsing in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,66 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 #include "headers/graph.hpp"
 
-int main(int argc, char *argv[]){
-    Graph g = Graph(argv[1]);
-    //g.print_verteces();
-    g.bfs(86771, 110636);
+char* getCmdOption(char ** begin, char ** end, const std::string & option);
+bool cmdOptionExists(char** begin, char** end, const std::string& option);
 
-    // g.print_edges();
-    g.summary();
+
+int main(int argc, char *argv[]){
+    
+    if(cmdOptionExists(argv, argv+argc, "--file") && 
+        cmdOptionExists(argv, argv+argc, "--start") &&
+        cmdOptionExists(argv, argv+argc, "--end") &&
+        cmdOptionExists(argv, argv+argc, "--algorithm")
+    ) {
+        string file_name = getCmdOption(argv, argv + argc, "--file");
+        try{
+            int vstart = stoi(getCmdOption(argv, argv + argc, "--start"));
+            int vend = stoi(getCmdOption(argv, argv + argc, "--end"));
+
+            string algorithm = getCmdOption(argv, argv + argc, "--algorithm");
+
+            Graph g = Graph(file_name);
+
+            g.bfs(vstart, vend);
+            // g.print_verteces();
+            // g.print_edges();
+            // g.summary();
+
+        }catch (const invalid_argument e) {
+            std::cout << "Problem while parsing arguments:" <<std::endl;
+            std::cout << "\n\t" << e.what() << endl;
+            return 0;
+        }
+        
+        
+
+    }else{
+        std::cout << "Not enough arguments passed. 4 arguments needed with following mandatory options: \n" 
+        << "\t--filename specifies the data file\n"
+        << "\t--start defines the starting node\n"
+        << "\t--end defines the ending node\n"
+        << "\t--algorithm defines the preferred search algorithm" 
+        << std::endl;
+    }
+}
+
+
+
+
+char* getCmdOption(char ** begin, char ** end, const std::string & option)
+{
+    char ** itr = std::find(begin, end, option);
+    if (itr != end && ++itr != end)
+    {
+        return *itr;
+    }
+    return 0;
+}
+
+bool cmdOptionExists(char** begin, char** end, const std::string& option)
+{
+    return std::find(begin, end, option) != end;
 }
