8b5ce4aac534067e6e97da0b1c61397b3b444b41
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2023-03-28 20:46:35 +0200

    rm unused

--- messagebox.cpp
+++
@@ -1,8 +0,0 @@
-#include "messagebox.h"
-
-void MessageBox::show(const QString& title, const QString& message) {
-    QMessageBox msgBox;
-    msgBox.setWindowTitle(title);
-    msgBox.setText(message);
-    msgBox.exec();
-}
--- src/headers/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-#include "graph.hpp"
-
-void displayMessage(const char* message);
-Graph loadGraph(std::string fileName);
-
-#endif // DISPLAY_H
